8e3f122b13f5014b5a1309b232acd57648c83155
ravilella1234 <masters.selenium@gmail.com>
2021-09-14 19:05:45 +0530

    Browser Drivers Path setup in different approaches

--- September6PM_SeleniumBatch/src/com/launchings/BrowserLaunch.java
+++ September6PM_SeleniumBatch/src/com/launchings/BrowserLaunch.java
@@ -1,18 +1,28 @@
 package com.launchings;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserLaunch
 {
 
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ravi\\Desktop\\drivers\\chromedriver.exe");
-		ChromeDriver  driver = new ChromeDriver();
+		WebDriver driver= null;
 		
-		System.setProperty("webdriver.gecko.driver", "C:/Users/ravi/Desktop/drivers/geckodriver.exe");
-		FirefoxDriver driver1 = new FirefoxDriver();
+		//WebDriverManager.chromedriver().setup();
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\ravi\\Desktop\\drivers\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\drivers\\chromedriver.exe");
+		 driver = new ChromeDriver();
+		 driver.get("https://www.amazon.in");
+		
+		WebDriverManager.firefoxdriver().setup();
+		//System.setProperty("webdriver.gecko.driver", "C:/Users/ravi/Desktop/drivers/geckodriver.exe");
+		 driver = new FirefoxDriver();
+		 driver.get("https://www.bestbuy.com");
 		
 		//webdriver.ie.driver
 		//webdriver.edge.driver
---
+++ September6PM_SeleniumBatch/src/com/launchings/TC_002.java
@@ -0,0 +1,13 @@
+package com.launchings;
+
+public class TC_002 extends BaseTest
+{
+
+	public static void main(String[] args) 
+	{
+		launch("firefox");
+		
+		navigateUrl("https://www.bestbuy.com");
+	}
+
+}
