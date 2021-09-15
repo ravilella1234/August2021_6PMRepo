513215a36956b19cbefc772555c02047683174b6
ravilella1234 <masters.selenium@gmail.com>
2021-09-15 18:54:22 +0530

    Properties Class usage for storing the test data

--- September6PM_SeleniumBatch/src/com/launchings/BaseTest.java
+++ September6PM_SeleniumBatch/src/com/launchings/BaseTest.java
@@ -1,5 +1,8 @@
 package com.launchings;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -9,15 +12,40 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest
 {
 	public static WebDriver driver;
+	public static String projectPath = System.getProperty("user.dir");
+	public static FileInputStream fis;
+	public static Properties p;
+	public static Properties mainProp;
+	public static Properties childProp;
+	
+	public static void init() throws Exception
+	{
+		fis = new FileInputStream(projectPath+"\\data.properties");
+		p = new Properties();
+		p.load(fis);
+		//System.out.println(p.getProperty("chromebrowser"));
+		//System.out.println(p.getProperty("amazonurl"));
+		
+		fis = new FileInputStream(projectPath+"\\environment.properties");
+		mainProp = new Properties();
+		mainProp.load(fis);
+		String e = mainProp.getProperty("env");
+		System.out.println(e);
+		
+		fis = new FileInputStream(projectPath+"\\"+e+".properties");
+		childProp = new Properties();
+		childProp.load(fis);
+		System.out.println(childProp.getProperty("amazonurl"));
+	}
 	
 	public static void launch(String browser)
 	{
-		if(browser.equals("chrome"))
+		if(p.getProperty(browser).equals("chrome"))
 		{
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}
-		else if(browser.equals("firefox"))
+		else if(p.getProperty(browser).equals("firefox"))
 		{
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
@@ -26,7 +54,7 @@ public class BaseTest
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(url);
+		driver.get(childProp.getProperty(url));
 	}
 
 }
