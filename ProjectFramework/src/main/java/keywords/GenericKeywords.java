b5136a86a79cd95fbfdcd9913bec19b43b11b54f
ravilella1234 <masters.selenium@gmail.com>
2021-11-16 19:27:34 +0530

    Project Day-1

---
+++ ProjectFramework/src/main/java/keywords/GenericKeywords.java
@@ -0,0 +1,38 @@
+package keywords;
+
+public class GenericKeywords 
+{
+
+	 public void openBrowser()
+	 {
+		 
+	 }
+	 
+	 public void navigate()
+	 {
+		 
+	 }
+	 
+	 public void type()
+	 {
+		 
+	 }
+	 
+	 public void click()
+	 {
+		 
+	 }
+	 
+	 public void select()
+	 {
+		 
+	 }
+	 
+	 public String getText()
+	 {
+		 
+		return null; 
+	 }
+	
+	
+}
---
+++ ProjectFramework/src/main/java/keywords/ValidationKeywords.java
@@ -0,0 +1,26 @@
+package keywords;
+
+public class ValidationKeywords 
+{
+	
+	public void validateTitle()
+	{
+		
+	}
+	
+	public void validateText()
+	{
+		
+	}
+	
+	public void validateElementPresent()
+	{
+		
+	}
+	
+	public void validateUrl()
+	{
+		
+	}
+
+}
